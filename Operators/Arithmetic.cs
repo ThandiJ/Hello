a3b3eb5db2494c6d8e204599dc9dd3ee5caf62b5
Thandi.Motaung <Thandi.Motaung@fdmgroup.com>
2022-07-08 13:00:25 +0200

    changes to arithmetic class

--- Operators/Arithmetic.cs
+++ Operators/Arithmetic.cs
@@ -38,7 +38,7 @@ namespace Hello.Operators
             int number1 = Convert.ToInt32(num1);
             int number2 = Convert.ToInt32(num2);
             int result1,result2;
-            StringBuilder sb = new("\tUnary Operator\n*************Increment************\n");
+            StringBuilder sb = new("\n");
             result1 = number1++;
             sb.AppendLine($"Post Increment: number is {number1} and result is {result1}");
 
